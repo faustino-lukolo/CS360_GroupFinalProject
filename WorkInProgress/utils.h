946bf1863a646899ceb340e6cce75fbea55224d0
mohamedabdi206 <itismemoe@gmail.com>
2016-04-18 00:27:20 -0700

    removed commit test message

--- WorkInProgress/utils.h
+++ WorkInProgress/utils.h
@@ -4,7 +4,6 @@
 #include "type.h"
 #include <stdlib.h>
 
-// THIS IS A TEST OF COMMIT AND PUSH
 /************************ globals *****************************/
 char cmd[32], pathname[128], params[64], cwdname[128], blkBuf[BLOCK_SIZE], line[1024];
 int ninodes, nblocks, ifree, bfree, InodeBeginBlock, nproc, dev;
